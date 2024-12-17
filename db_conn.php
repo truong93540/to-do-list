038c6397d3f7815672e703153600b189376c31bd
truong93540 <truong93540@gmai.com>
2024-12-17 12:44:01 +0700

    2

--- db_conn.php
+++ db_conn.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:defgh.database.windows.net,1433; Database = truong111", "truong123", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:defgh.database.windows.net,1433; Database = truong111", "truong123", "truong93540@");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "truong123", "pwd" => "{your_password_here}", "Database" => "truong111", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "truong123", "pwd" => "truong93540@", "Database" => "truong111", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:defgh.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
