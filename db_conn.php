1312d30bd66f6a3ecd8d04fc20591e6edec7370a
truong93540 <truong93540@gmai.com>
2024-12-17 12:31:38 +0700

    17/21/2024

--- db_conn.php
+++ db_conn.php
@@ -1,14 +1,16 @@
-<?php 
-
-$sName = "localhost";
-$uName = "root";
-$pass = "";
-$db_name = "to_do_list";
-
+<?php
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
-                    $uName, $pass);
+    $conn = new PDO("sqlsrv:server = tcp:defgh.database.windows.net,1433; Database = truong111", "truong123", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-  echo "Connection failed : ". $e->getMessage();
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "truong123", "pwd" => "{your_password_here}", "Database" => "truong111", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:defgh.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
